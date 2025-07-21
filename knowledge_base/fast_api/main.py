61936ff60546e8189912d5ef48ace6f2687e2d6d
o.boykov <xponenc@gmail.com>
2025-07-21 16:07:30 +0400

    приведение в порядок индикатора celery-progress, выкатывание в продакт бота и api

--- knowledge_base/fast_api/main.py
+++ knowledge_base/fast_api/main.py
@@ -24,8 +24,8 @@ app = FastAPI(
     title="Knowledge Base AI API",
     description="API для взаимодействия с Telegram-ботом. Поддерживает обработку сообщений пользователей, генерацию ответов AI и управление профилями пользователей. Все эндпоинты доступны под префиксом `/api`.",
     version="1.0.0",
-    docs_url="/api/docs",  # Swagger UI
-    redoc_url="/api/redoc"  # ReDoc
+    docs_url="/docs",  # Swagger UI
+    redoc_url="/redoc"  # ReDoc
 )
 
 
