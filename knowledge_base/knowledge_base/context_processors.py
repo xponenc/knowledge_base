1a13967ac71ba0fd8403fadf2684c3960d15ef8f
o.boykov <xponenc@gmail.com>
2025-09-03 10:52:25 +0400

    вывод ip пользователя

--- knowledge_base/knowledge_base/context_processors.py
+++ knowledge_base/knowledge_base/context_processors.py
@@ -1,5 +1,13 @@
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0].strip()  # Первый IP в списке — клиентский
+    else:
+        ip = request.META.get('REMOTE_ADDR', 'Unknown')
+    return ip
+
 def request_data(request):
     return {
-        'client_ip': request.META.get('REMOTE_ADDR', 'Unknown'),
+        'client_ip': get_client_ip(request),
         'user_agent': request.META.get('HTTP_USER_AGENT', 'Unknown'),
     }
